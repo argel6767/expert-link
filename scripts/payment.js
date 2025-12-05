bafcbbecdb3d440f7ccfbc00e8a4f4c22921c72b
Kyle <kyle@Kyles-MacBook-Air.local>
2025-12-05 10:00:24 -0500

    become-specialist finished

--- scripts/payment.js
+++ scripts/payment.js
@@ -9,25 +9,25 @@ document.addEventListener('DOMContentLoaded', () => {
                     <div class="containers-row">
                         <div class="container-column">
                             <label for="fname">First Name:</label>
-                            <input type="text" id="fname" placeholder="First Name*"/>
+                            <input type="text" id="fname" placeholder="First Name*" class="pay-input"/>
                         </div>
                         <div class="container-column">
                             <label for="Lname">Last Name:</label>
-                            <input type="text" id="Lname" placeholder="Last Name*"/>
+                            <input type="text" id="Lname" placeholder="Last Name*" class="pay-input"/>
                         </div>
                     </div>
                     <div class="container-column">
                         <label for="card-number">Card Number:</label>
-                        <input type="text" id="card-number" placeholder="Type Card # Here*"/>
+                        <input type="text" id="card-number" placeholder="Type Card # Here*" class="pay-input"/>
                     </div>
                     <div class="containers-row">
                         <div class="container-column">
                             <label for="exp-date">Exp Date:</label>
-                            <input type="text" id="exp-date" placeholder="MM/YY*"/>
+                            <input type="text" id="exp-date" placeholder="MM/YY*" class="pay-input"/>
                         </div>
                         <div class="container-column">
                             <label for="cvv">CVV:</label>
-                            <input type="text" id="cvv" placeholder="CVV*"/>
+                            <input type="text" id="cvv" placeholder="CVV*" class="pay-input"/>
                         </div>
                     </div>
                 `;
